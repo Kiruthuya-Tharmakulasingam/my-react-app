97b8e5b7c6df06aec6afc2521965a95168e60cfb
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-15 14:40:01 +0530

    add some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <>
+      <h2 className="header">Header section edited</h2>
+    </>
+  );
+}
+
+function Welcome() {
+  return (
+    <>
+      <div className="welcome">
+        <h2>Body section</h2>
+        <h2>Welcome To Uki</h2>
+      </div>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <>
+      <h2 className="footer">Footer section</h2>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <div>
-        <h1>React application</h1>
+        <Header />
+        <Welcome />
+        <Footer />
       </div>
     </>
   );
